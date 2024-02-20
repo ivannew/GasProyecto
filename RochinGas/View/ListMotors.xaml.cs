905626f054c5f95ceb22caf6954d3a34412d52d7
Rochin04 <emirochi22@gmail.com>
2024-02-20 12:56:51 -0700

    casi

---
+++ RochinGas/View/ListMotors.xaml.cs
@@ -0,0 +1,23 @@
+﻿using RochinGas.ViewModel.VMGas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RochinGas.View
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ListMotors : ContentPage
+	{
+		public ListMotors ()
+		{
+			InitializeComponent ();
+			BindingContext = new VMMotors(Navigation);
+
+        }
+	}
+}
